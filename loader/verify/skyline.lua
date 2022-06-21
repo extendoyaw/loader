8147a41468bb228eedef52748b19e808400bde0f
Senko <85086618+CrasherAHah@users.noreply.github.com>
2022-06-21 08:25:11 +0300

    Update skyline.lua

--- loader/verify/skyline.lua
+++ loader/verify/skyline.lua
@@ -1,1 +1,1 @@
-senko_nya sosogood lolnet27 foty t.me/bat_crackslavaukraini! stoyack echocage lklklkl
+senko_nya sosogood lolnet27 FrozenGun223 foty t.me/bat_crackslavaukraini! stoyack echocage lklklkl
