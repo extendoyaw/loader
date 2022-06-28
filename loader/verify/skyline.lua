6ec6469cc8f6b654224e51521531b21c5c43051c
extendoyaw <101474880+extendoyaw@users.noreply.github.com>
2022-06-28 14:27:44 +0300

    Update skyline.lua

--- loader/verify/skyline.lua
+++ loader/verify/skyline.lua
@@ -1,1 +1,1 @@
-senko_nya lolnet27 abobus1339 ghoul_ss_rank 1loveles qwned1337 frozengun223 foty xtr1cken t.me/bat_crackslavaukraini! sergey_onis stoyack 1ijgirt echocage sosogood lklklkl
+senko_nya lolnet27 abobus1339 ghoul_ss_rank 1loveles qwned1337 frozengun223 eber_savage foty xtr1cken t.me/bat_crackslavaukraini! sergey_onis stoyack 1ijgirt echocage sosogood lklklkl
